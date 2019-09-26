9983e9154e70d98157c78e7b689d2ee706f8b898
Trieu Tran <trieutran91@users.noreply.github.com>
2019-09-26 18:56:20 +0200

    minor changes

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -4,18 +4,18 @@ import { Component, OnInit } from '@angular/core';
   selector: 'pm-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css'],
-  template: `
-    <nav class='navbar navbar-expand navbar-light bg-light'>
-        <a class='navbar-brand'>{{pageTitle}}</a>
-        <ul class='nav nav-pills'>
-          <li><a class='nav-link' routerLinkActive='active' [routerLink]="['/welcome']">Home</a></li>
-          <li><a class='nav-link' routerLinkActive='active' [routerLink]="['/products']">Cat List</a></li>
-        </ul>
-    </nav>
-    <div class='container'>
-      <router-outlet></router-outlet>
-    </div>
-    `
+  // template: `
+  //   <nav class='navbar navbar-expand navbar-light bg-light'>
+  //       <a class='navbar-brand'>{{pageTitle}}</a>
+  //       <ul class='nav nav-pills'>
+  //         <li><a class='nav-link' routerLinkActive='active' [routerLink]="['/welcome']">Home</a></li>
+  //         <li><a class='nav-link' routerLinkActive='active' [routerLink]="['/products']">Cat List</a></li>
+  //       </ul>
+  //   </nav>
+  //   <div class='container'>
+  //     <router-outlet></router-outlet>
+  //   </div>
+  //   `
 })
 export class NavbarComponent implements OnInit {
   pageTitle = 'Catshop';
