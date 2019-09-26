2eeb6c5e4f2881ba7f644219e72b6ce99758dcc5
Rebekka Aadland <rebekka.v.aa@gmail.com>
2019-09-26 15:37:22 +0200

    navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,12 @@ declare var payex: any;
 @Component({
   selector: 'pm-root',
   template: `
-    <nav class='navbar navbar-expand navbar-light bg-light'>
+  
+    <nav class='navbar navbar-expand-lg navbar-dark bg-light'>
+    <ul class='nav nav-pills'>
+    <li><a class='nav-link' routerLinkActive='active' [routerLink]="['/products']"><-</a></li>
+    </ul>
         <a class='navbar-brand'>{{pageTitle}}</a>
-        <ul class='nav nav-pills'>
-          <li><a class='nav-link' routerLinkActive='active' [routerLink]="['/welcome']">Home</a></li>
-          <li><a class='nav-link' routerLinkActive='active' [routerLink]="['/products']">Product List</a></li>
-        </ul>
     </nav>
     <div class='container'>
       <router-outlet></router-outlet>
@@ -17,7 +17,7 @@ declare var payex: any;
   styleUrls: ['./app.component.css']
 })
 export class AppComponent{
-  pageTitle = 'Acme Product Management';
+  pageTitle = 'Ketchup';
 
 
 }
