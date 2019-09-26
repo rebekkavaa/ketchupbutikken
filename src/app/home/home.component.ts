b9eba8b1f9f6f39389e2afe3b071bea2b86d5414
Rebekka Aadland <rebekka.v.aa@gmail.com>
2019-09-26 14:52:50 +0200

    home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,21 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProductService } from '../services/product.service';
 
 @Component({
-  templateUrl: './home.component.html'
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
   public pageTitle = 'Welcome';
+
+    constructor(private route: ActivatedRoute,
+              private router: Router,
+  ) {
+  }
+
+  onEnter(): void {
+    this.router.navigate(['/products']);
+  }
+ 
 }
